85c3df5028490cb67e0970d1790cb4165f8952f0
RedstoneWizard08 <94275204+RedstoneWizard08@users.noreply.github.com>
2024-05-06 23:57:57 +0000

    Deploying to gh-pages from @ RedstoneWizard08/ConfigurableWarning@15ad503a7d334229b18bbf761895886381a6ca74 🚀

---
+++ public/main.js
@@ -0,0 +1,10 @@
+export default {
+    defaultTheme: 'dark',
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/RedstoneWizard08/ConfigurableWarning',
+            title: 'GitHub'
+        }
+    ]
+}

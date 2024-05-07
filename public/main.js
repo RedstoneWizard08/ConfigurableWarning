f3ce8da72657ce3caddcd21480f9f40291920582
RedstoneWizard08 <94275204+RedstoneWizard08@users.noreply.github.com>
2024-05-07 19:08:58 +0000

    Deploying to gh-pages from @ RedstoneWizard08/ConfigurableWarning@e51854b41baf6d3b9598ec5ed504176da9f76f7d 🚀

--- public/main.js
+++ public/main.js
@@ -5,6 +5,11 @@ export default {
             icon: 'github',
             href: 'https://github.com/RedstoneWizard08/ConfigurableWarning',
             title: 'GitHub'
-        }
-    ]
-}
+        },
+        {
+            icon: "download",
+            href: "https://thunderstore.io/c/content-warning/p/RedstoneWizard08/ConfigurableWarning/",
+            title: "Thunderstore"
+        },
+    ],
+};

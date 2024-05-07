82df52ed1a1f9dac6e054a592dc7db047b6281e7
RedstoneWizard08 <94275204+RedstoneWizard08@users.noreply.github.com>
2024-05-07 00:15:05 +0000

    Fix docs

--- Build/Source/Tasks/DocsTask.cs
+++ Build/Source/Tasks/DocsTask.cs
@@ -6,6 +6,7 @@ namespace ConfigurableWarning.Build.Tasks;
 [TaskName("Docs")]
 public sealed class DocsTask : FrostingTask<BuildContext> {
     public override void Run(BuildContext context) {
+        context.DocFxMetadata("../docs/docfx.json");
         context.DocFxBuild("../docs/docfx.json");
     }
 }
